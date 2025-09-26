e7fb89dfbf48a07477c173de774fb65a5e8a823e
Mohammed Najath <najathsidhelp@gmail.com>
2025-09-26 21:13:49 +0530

    Added HelloApp.java file

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+public class HelloApp{
+	public static void main(String[] args){
+	if(args.length >0 && args[0].equals("fail"){
+	System.out.println("Failure Scenario triggered!");
+}
+else{
+System.out.println("Success Scenario triggered!");
+}
+}
+}
